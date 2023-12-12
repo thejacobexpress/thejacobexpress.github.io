ddfa46940987c57f6d9daed57698d169fd07e8a1
Jacob <jacobmckee06@gmail.com>
2023-12-11 16:13:40 -0800

    Added about section and made small adjustments

--- homeScript.js
+++ homeScript.js
@@ -1,4 +1,4 @@
-const elements = document.querySelectorAll('.projectsText');
+const elements = document.querySelectorAll('.projectsText, .aboutText');
 const options = {
   root: null,
   rootMargin: '0px',
@@ -6,7 +6,7 @@ const options = {
 }
 const callbacks = (entries) => {
   entries.forEach(entry => {
-    if (entry.isIntersecting){
+    if(entry.isIntersecting){
       entry.target.classList.add('transitionUp');
     }
   });
@@ -14,4 +14,40 @@ const callbacks = (entries) => {
 let observer = new IntersectionObserver(callbacks, options);
 elements.forEach(element => {
   observer.observe(element);
+});
+
+const lElements = document.querySelectorAll('.aboutParagraph');
+const lOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.9
+}
+const lCallbacks = (entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('transitionLeft');
+    }
+  });
+}
+let lObserver = new IntersectionObserver(lCallbacks, lOptions);
+lElements.forEach(element => {
+  lObserver.observe(element);
+});
+
+const rElements = document.querySelectorAll('.aboutImage');
+const rOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.9
+}
+const rCallbacks = (entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('transitionRight');
+    }
+  });
+}
+let rObserver = new IntersectionObserver(rCallbacks, rOptions);
+rElements.forEach(element => {
+  rObserver.observe(element);
 });
