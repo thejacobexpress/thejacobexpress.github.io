a497854b3596008d0379ab5e961a3903df7a00e1
Jacob <jacobmckee06@gmail.com>
2024-01-01 18:53:34 -0800

    Optimized project section placeholder for mobile

--- projectsScript.js
+++ projectsScript.js
@@ -1,3 +1,5 @@
+// var isMobile = !!navigator.userAgent.match(/iphone|android|samsung|galaxy|pixel|blackberry/ig) || false;
+
 // const project1Button1El = document.getElementById("project1Button1");
 // const project1Button2El = document.getElementById("project1Button2");
 // const project1Button3El = document.getElementById("project1Button3");
@@ -5,7 +7,11 @@
 // const project1Buttons = [project1Button1El, project1Button2El, project1Button3El];
 
 // var selectedButton = project1Button1El;
-// project1Button1El.classList.add("selectButton");
+// if(isMobile) {
+//     project1Button1El.classList.add("selectButtonMobile");
+// } else {
+//     project1Button1El.classList.add("selectButton");
+// }
 
 // const project1Section1 = document.getElementById("project1ImageTextDiv1");
 // const project1Section2 = document.getElementById("project1ImageTextDiv2");
@@ -25,20 +31,27 @@
 //             if(i == project1Sections.length-1) {
 //                 changeSection(project1Sections[0]);
 //                 selectedButton = project1Buttons[0];
-//                 project1Button1El.classList.add("selectButton");
+//                 if(isMobile) {
+//                     project1Button1El.classList.add("selectButtonMobile");
+//                 } else {
+//                     project1Button1El.classList.add("selectButton");
+//                 }
 //                 unselectButtons();
 
 //                 selectedSection = project1Sections[0];
-//                 break;
 //             } else {
 //                 changeSection(project1Sections[i+1]);
 //                 selectedButton = project1Buttons[i+1];
-//                 project1Buttons[i+1].classList.add("selectButton");
+//                 if(isMobile) {
+//                     project1Buttons[i+1].classList.add("selectButtonMobile");
+//                 } else {
+//                     project1Buttons[i+1].classList.add("selectButton");
+//                 }
 //                 unselectButtons();
 
 //                 selectedSection = project1Sections[i+1];
-//                 break;
 //             }
+//             break;
 //         }
 //     }
 // }
@@ -50,7 +63,11 @@
     
 //     changeSection(project1Section1);
 //     selectedButton = document.getElementById("project1Button1");
-//     project1Button1El.classList.add("selectButton");
+//     if(isMobile) {
+//         project1Button1El.classList.add("selectButtonMobile");
+//     } else {
+//         project1Button1El.classList.add("selectButton");
+//     }
 //     unselectButtons();
 // }
 // function project1Button2() {
@@ -60,7 +77,11 @@
 
 //     changeSection(project1Section2);
 //     selectedButton = document.getElementById("project1Button2");
-//     project1Button2El.classList.add("selectButton");
+//     if(isMobile) {
+//         project1Button2El.classList.add("selectButtonMobile");
+//     } else {
+//         project1Button2El.classList.add("selectButton");
+//     }
 //     unselectButtons();
 // }
 // function project1Button3() {
@@ -70,7 +91,11 @@
 
 //     changeSection(project1Section3);
 //     selectedButton = document.getElementById("project1Button3");
-//     project1Button3El.classList.add("selectButton");
+//     if(isMobile) {
+//         project1Button3El.classList.add("selectButtonMobile");
+//     } else {
+//         project1Button3El.classList.add("selectButton");
+//     }
 //     unselectButtons();
 // }
 
@@ -101,8 +126,10 @@
 
 // function unselectButtons() {
 //     for(let i = 0; i < project1Buttons.length; i++) {
-//         if(project1Buttons[i] != selectedButton) {
+//         if(project1Buttons[i] != selectedButton && !isMobile) {
 //             project1Buttons[i].classList.remove("selectButton");
+//         } else if(project1Buttons[i] != selectedButton && isMobile) {
+//             project1Buttons[i].classList.remove("selectButtonMobile");
 //         }
 //     }
 // }
